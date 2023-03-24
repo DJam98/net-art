8f1c34d955ef8a1d225015ba647fd0df50073690
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-03-24 17:38:39 +1100

    Testing

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,21 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+let y = -200
+
 function setup () {
    createCanvas (innerWidth, innerHeight)
 }
 
-function  draw () {
-   background (`turquoise`)
-}
+function draw() {
+   background('#40E0D0');
+   let c = color('pink');
+   fill(c);
+   noStroke();  
+   square(100, y, 200)
+   y = y + 2
+   
+   if (y > innerHeight) {
+     y = -200
+   }
+ }
