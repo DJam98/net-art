9840a8ff293294717cb37db41b0b5d63f0d69a5c
DJam98 <djam98@otd.ink>
2023-04-28 18:43:30 +1000

    Class Stuff

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,67 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-let y = -200
+// let y = -200
 
-function setup () {
-   createCanvas (innerWidth, innerHeight)
+// function setup () {
+//    createCanvas (innerWidth, innerHeight)
+// }
+
+// function draw() {
+//    background('#40E0D0');
+//    let c = color('pink');
+//    fill(c);
+//    noStroke();  
+//    square(100, y, 200)
+//    y = y + 2
+   
+//    if (y > innerHeight) {
+//      y = -200
+//    }
+//  }
+
+const cnv = document.getElementById ("cnv_id")
+
+cnv.width = innerWidth
+cnv.height = innerHeight
+
+console.dir (cnv)
+
+const squuares = []
+
+cnv.onpointerover = e => {
+mouse_pos.x = e.x
+mouse_pos.y = e.y
 }
 
-function draw() {
-   background('#40E0D0');
-   let c = color('pink');
-   fill(c);
-   noStroke();  
-   square(100, y, 200)
-   y = y + 2
-   
-   if (y > innerHeight) {
-     y = -200
-   }
- }
+const ctx = cnv.getContext (`2d`)
+
+cnv.onpointerdown = e => {
+  // console.log (`Achivement Unlocked: Interaction`)
+  // console.dir (e)
+  console.log (e.x, e.y)
+  
+  squuares.push ({
+    x: e.x,
+    y: e.y
+  })
+
+}
+
+requestAnimationFrame (drawFrame)
+
+function drawFrame () {
+  ctx.fillStyle = `turquoise`
+  ctx.fillRect (0, 0, cnv. width, cnv.height)
+
+  squuares.forEach (s => {
+    s.x += Math.random()*6-3
+    s.y += Math.random()*6-3
+    ctx.fillStyle = `purple`
+    ctx.fillRect (s.x - 50, s.y- 50, 100, 100)
+  })
+
+  requestAnimationFrame (drawFrame)
+
+
+}
