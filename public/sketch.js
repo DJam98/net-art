af3d44eba97e73258ddfed36b4af9e8109fa046a
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-05-16 13:20:06 +1000

    Stuff

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,42 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-let y = -200
+const cnv = document.getElementById (`example_canvas`)
+cnv.style.background = "turquoise"
 
-function setup () {
-   createCanvas (innerWidth, innerHeight)
+const ctx = cnv.getContext (`2d`)
+
+resize_canvas ()
+
+window.onresize = resize_canvas
+
+let y_pos = cnv.height / 2 - 50
+
+requestAnimationFrame (draw_frame)
+
+function draw_frame () {
+   ctx.fillStyle = `black`
+   ctx.fillRect (0, 0, cnv.width, cnv.height)
+
+   ctx.fillStyle = `white`
+   ctx.fillRect (150, y_pos, 100, 100)
+   
+
+   // y_pos += 0.005
+
+   // if (y_pos > cnv.width) {
+   //    y_pos = 0
+   // }
+
+   requestAnimationFrame (draw_frame)
 }
 
-function draw() {
-   background('#40E0D0');
-   let c = color('pink');
-   fill(c);
-   noStroke();  
-   square(100, y, 200)
-   y = y + 2
-   
-   if (y > innerHeight) {
-     y = -200
-   }
- }
+function mouse (){
+
+
+}
+
+function resize_canvas () {
+   cnv.width  = innerWidth
+   cnv.height = innerHeight   
+}
